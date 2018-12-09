8e1f89f58e89dc2950aa971ee56653a22d75e9a2
Omri Bar-Zik <omri@bar-zik.com>
2018-12-09 21:45:29 +0200

    added jsdoc to logger

--- src/logger.js
+++ src/logger.js
@@ -2,14 +2,29 @@ const chalk = require( 'chalk' );
 
 const consoleLog = console.log; // eslint-disable-line no-console
 
+/**
+ * prints to console in red.
+ * 
+ * @param {string} errorMessage the message that is printed.
+ */
 function error( errorMessage ) {
 	consoleLog( chalk.red( errorMessage ) );
 }
 
+/**
+ * prints to console in yellow.
+ * 
+ * @param {string} warnMessage the message that is printed.
+ */
 function warn( warnMessage ) {
 	consoleLog( chalk.yellow( warnMessage ) );
 }
 
+/**
+ * prints to console in white.
+ * 
+ * @param {string} logMessage the message that is printed.
+ */
 function log( logMessage ) {
 	consoleLog( logMessage );
 }
