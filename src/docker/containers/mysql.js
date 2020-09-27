c3002a8bd2c48d37db1ef99c7a3156ba4a506f78
Omri Bar-Zik <omri@bar-zik.com>
2020-09-27 20:06:20 +0300

    Add: mysql continer preset

--- src/docker/containers/mysql.js
+++ src/docker/containers/mysql.js
@@ -1,19 +1,24 @@
-const Container = require('./container')
+const { Docker } = require('../docker')
+// const Container = require('./container')
 
-class MysqlContainer extends Container {
-  constructor (name, port) {
-    super({
-      image: 'mysql:5.7',
-      name: `cywp-${name}-mysql`,
-      network: 'cywp-network',
-      exposePorts: [
-        { host: port, docker: 3306 }
-      ],
-      environmentVariables: [
-        { name: 'MYSQL_ROOT_PASSWORD', value: 'cywp' }
-      ]
-    })
-  }
+/**
+ * Create mysql continer.
+ * @param {string} name - The name of the container.
+ * @param {number} port - The port expose to the host.
+ * @returns {Promise<Container>}
+ */
+function CreateMysqlContainer (name, port, run = false) {
+  return Docker.prototype.CreateContainer({
+    image: 'mysql:5.7',
+    name: `cywp-${name}-mysql`,
+    network: 'cywp-network',
+    exposePorts: [
+      { host: port, docker: 3306 }
+    ],
+    environmentVariables: [
+      { name: 'MYSQL_ROOT_PASSWORD', value: 'cywp' }
+    ]
+  }, run)
 }
 
-module.exports = MysqlContainer
+module.exports = { CreateMysqlContainer }
