010fb76152644a62ea1043a128b8e069b7cd2c5d
Omri Bar-Zik <omri@bar-zik.com>
2020-10-09 14:11:16 +0300

    Add: rm method to container

--- src/docker/containers/container.js
+++ src/docker/containers/container.js
@@ -15,25 +15,41 @@ class Container {
    * @returns {Promise<Container>} Return the current conitner.
    */
   start () {
-    let stderr = ''
-
     const start = spawn('docker', ['container', 'start', this.options.dockerId])
 
-    start.stderr.on('data', (data) => {
-      stderr += data
-    })
+    return this.ReturnPromise(start, () => { this.options.status = 'started' })
+  }
 
-    return this.ReturnPromise(start, stderr, () => { this.options.status = 'started' })
+  /**
+   * Remove the continer
+   * @returns {Promise<Container>} Return the current conitner.
+   */
+  rm (force = false, volumes = true) {
+    const rmArgs = ['container', 'rm']
+
+    if (force) { rmArgs.push('--force') }
+    if (volumes) { rmArgs.push('--volumes') }
+
+    rmArgs.push(this.options.dockerId)
+
+    const rm = spawn('docker', rmArgs)
+
+    return this.ReturnPromise(rm, () => { this.options.status = 'removed' })
   }
 
   /**
    * Return the chiild process as promise.
    *
    * @param {import('child_process').ChildProcessWithoutNullStreams} process the child process thats running.
-   * @param {string} stderr the error massage the process throw.
    * @param {Function} callback callback that run in the process is succsesful.
    */
-  ReturnPromise (process, stderr, callback) {
+  ReturnPromise (process, callback) {
+    let stderr = ''
+
+    process.stderr.on('data', (data) => {
+      stderr += data
+    })
+
     return new Promise((resolve, reject) => {
       process.on('close', (code) => {
         if (code) {
