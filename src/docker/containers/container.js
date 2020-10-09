552fab1d32ef7c46b0e6f53b88dd8d067365fad7
Omri Bar-Zik <omri@bar-zik.com>
2020-10-09 14:50:36 +0300

    Add: stop and logs to container class

--- src/docker/containers/container.js
+++ src/docker/containers/container.js
@@ -17,7 +17,10 @@ class Container {
   start () {
     const start = spawn('docker', ['container', 'start', this.options.dockerId])
 
-    return this.ReturnPromise(start, () => { this.options.status = 'started' })
+    return this.ReturnPromise(start, () => {
+      this.options.status = 'started'
+      return this
+    })
   }
 
   /**
@@ -34,16 +37,68 @@ class Container {
 
     const rm = spawn('docker', rmArgs)
 
-    return this.ReturnPromise(rm, () => { this.options.status = 'removed' })
+    return this.ReturnPromise(rm, () => {
+      this.options.status = 'removed'
+      return this
+    })
+  }
+
+  /**
+   * Stop the continer.
+   *
+   * @param {number} time Seconds to wait for stop before stopping the container.
+   * @returns {Promise<Container>} Return the current conitner.
+   */
+  stop (time = 10) {
+    const stopArgs = ['container', 'stop']
+
+    stopArgs.push('--time', time)
+
+    const stop = spawn('docker', stopArgs)
+
+    return this.ReturnPromise(stop, () => {
+      this.options.status = 'stoped'
+      return this
+    })
+  }
+
+  /**
+   * Return the container logs
+   *
+   * @param {object} options - options object for logs.
+   * @param {string} options.since - Show logs since timestamp (e.g. 2020-01-02T13:23:37) or relative (e.g. 42m for 42 minutes)
+   * @param {string} options.tail - Number of lines to show from the end of the logs (default "all")
+   * @param {boolean} options.timestamps - show timestamps.
+   * @returns {string} Return the container logs.
+   */
+  logs (options) {
+    const logsAgrs = ['container', 'stop']
+    let stdout = ''
+
+    if (options.since) { logsAgrs.push('--since', options.since) }
+    if (options.tail) { logsAgrs.push('--tail', options.tail) }
+    if (options.timestamps) { logsAgrs.push('--since', options.timestamps) }
+
+    const logs = spawn('docker', logsAgrs)
+
+    logs.stdout.on('data', (data) => {
+      stdout += data
+    })
+
+    return this.ReturnPromise(logs, () => stdout)
   }
 
   /**
    * Return the chiild process as promise.
    *
    * @param {import('child_process').ChildProcessWithoutNullStreams} process the child process thats running.
-   * @param {Function} callback callback that run in the process is succsesful.
+   * @param {Function} callback callback that deterred what to return when the process is succsesful.
    */
   ReturnPromise (process, callback) {
+    if ('function' !== typeof callback) {
+      throw new TypeError('callback must be a function')
+    }
+
     let stderr = ''
 
     process.stderr.on('data', (data) => {
@@ -57,9 +112,7 @@ class Container {
           return
         }
 
-        callback()
-
-        resolve(this)
+        resolve(callback())
       })
     })
   }
