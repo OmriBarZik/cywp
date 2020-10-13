144fbd116940f93278bacbb324cde88cb44af0d7
Omri Bar-Zik <omri@bar-zik.com>
2020-10-13 08:05:23 +0300

    Fix: Tests

--- test/docker/docker.spec.js
+++ test/docker/docker.spec.js
@@ -153,7 +153,7 @@ describe('Docker', () => {
 
     it('should create runnig docker continer', async () => {
       const continer = await CreateContainer({ image: 'hello-world' }, true)
-      const continerCheck = spawnSync('docker', ['ps', '-a', '-q', '--filter', `id=${continer.options.dockerId}`, '--filter', 'status=exited'])
+      const continerCheck = spawnSync('docker', ['ps', '-a', '-q', '--filter', `id=${continer.options.dockerId}`])
 
       dockerIds.push(continer.options.dockerId)
 
