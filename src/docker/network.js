762555c50950ea013027faa976cf5fd1ee637228
Omri Bar-Zik <omri@bar-zik.com>
2020-10-16 13:32:18 +0300

    Add: rm and inspect

--- src/docker/network.js
+++ src/docker/network.js
@@ -1,20 +1,54 @@
+const { spawn } = require('child_process')
+const { ReturnPromise, CleanInspect } = require('./util')
 
 class Network {
   /**
    * Set info for the network.
    *
-   * @param {NetworkOption} options -the docker network options
+   * @param {NetworkOption} options - the docker network options
    */
   constructor (options) {
     this.options = options
   }
 
+  /**
+   * Remove the network
+   *
+   * @returns {Promise<Network>} Return the current network.
+   */
   rm () {
+    const rm = spawn('docker', ['network', 'rm', this.options.id])
 
+    return ReturnPromise(rm, () => {
+      this.options.status = 'dead'
+      return this
+    })
   }
 
-  inspect () {
+  /**
+   * Inspect current network.
+   *
+   * @param {string} [format] - Format the output using the given Go template.
+   * @returns {Promise<string>} Network info.
+   */
+  inspect (format) {
+    const inspectArgs = ['network', 'inspect']
 
+    let stdout = ''
+
+    if (format) { inspectArgs.push('--format', format) }
+
+    inspectArgs.push(this.options.id)
+
+    const inspect = spawn('docker', inspectArgs)
+
+    inspect.stdout.on('data', (data) => {
+      stdout += data
+    })
+
+    return ReturnPromise(inspect, () => {
+      return CleanInspect(stdout)
+    })
   }
 }
 
