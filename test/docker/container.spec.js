5ae20b490215e6aeb5b28ddd4f2b103e00d683e1
Omri Bar-Zik <omri@bar-zik.com>
2020-10-18 16:42:14 +0300

    Add: Tests

--- test/docker/container.spec.js
+++ test/docker/container.spec.js
@@ -67,6 +67,7 @@ describe('Container', () => {
       return container.logs().then((logs) => {
         expect(logs.stdout).toEqual(expect.stringContaining('docker'))
         expect(logs.stderr).toBeFalsy()
+        expect(logs.container).toBe(container)
       })
     })
 
@@ -76,6 +77,7 @@ describe('Container', () => {
       return container.logs({ tail: 2 }).then(logs => {
         expect(logs.stdout).toEqual(' https://docs.docker.com/get-started/\n\n')
         expect(logs.stderr).toBeFalsy()
+        expect(logs.container).toBe(container)
       })
     })
 
@@ -89,6 +91,7 @@ describe('Container', () => {
       return container.logs({ timeStamps: true }).then(logs => {
         expect(logs.stdout).toEqual(expect.stringContaining(time.toISOString().substr(0, 17)))
         expect(logs.stderr).toBeFalsy()
+        expect(logs.container).toBe(container)
       })
     })
 
@@ -102,6 +105,7 @@ describe('Container', () => {
       return container.logs({ since: time.toISOString() }).then(logs => {
         expect(logs.stdout).toBeTruthy()
         expect(logs.stderr).toBeFalsy()
+        expect(logs.container).toBe(container)
       })
     })
 
@@ -115,6 +119,7 @@ describe('Container', () => {
       return container.logs({ since: time.toISOString() }).then(logs => {
         expect(logs.stdout).toBeFalsy()
         expect(logs.stderr).toBeFalsy()
+        expect(logs.container).toBe(container)
       })
     })
   })
