d8531f6abb5e8156c879ea801e208653181cb031
Omri Bar-Zik <omri@bar-zik.com>
2020-10-25 12:37:42 +0200

    Add: file to contain pre maid comamnds

---
+++ src/workflow/sites-management.js
@@ -0,0 +1,23 @@
+const { CreateWordpressCliContainer } = require('../docker/presets/containers')
+
+/**
+ * Run wordpress cli command to initialize the wordpress sits.
+ *
+ * @param {import('../docker/container')} wordpress the wordpress container to initialize.
+ * @returns {Promise<import('../docker/container')>} wordpress cli container.
+ */
+function InitSite (wordpress) {
+  return CreateWordpressCliContainer(wordpress, [
+    'wp',
+    'core',
+    'install',
+    `--title=${wordpress.options.name}`,
+    '--admin_user=admin',
+    '--admin_password=password',
+    `--admin_email=${wordpress.options.name}@cywp.local`,
+    '--skip-email',
+    `--url=http://localhost:${wordpress.options.exposePorts[0].host}`,
+  ])
+}
+
+module.exports = { InitSite }
