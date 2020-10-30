af42ae9e87d37aee2e1f357e30e3512b26e8d712
Omri Bar-Zik <omri@bar-zik.com>
2020-10-31 00:18:09 +0200

    Add: RunInContainerOutput type

---
+++ src/wp-cli/types.js
@@ -0,0 +1,18 @@
+/**
+ * @typedef ThemeGetObject
+ * @property {string} name - The name of the theme.
+ * @property {string} title - the title of the theme.
+ * @property {string} version - version of the theme.
+ * @property {string} status - if the theme is active or inactive.
+ * @property {string} parent_theme - the name of the parent theme.
+ * @property {string} template_dir - path to the template directory.
+ * @property {string} stylesheet_dir - path to the stylesheet directory.
+ * @property {string} template - the name of the template file.
+ * @property {string} stylesheet - the name of the stylesheet file.
+ * @property {string} screenshot - theme screenshot.
+ * @property {string} description - theme description.
+ * @property {string} author - link of the theme author.
+ * @property {string[]} tags - list of the theme tags.
+ * @property {string} theme_root - path to theme root directory.
+ * @property {string} theme_root_uri - uri to theme root directory.
+ */
