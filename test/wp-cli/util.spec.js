f9291e0df5f367c59ec74ebcb403d5cabe955d57
Omri Bar-Zik <omri@bar-zik.com>
2020-11-09 18:01:48 +0200

    Add: Util tests

--- test/wp-cli/util.spec.js
+++ test/wp-cli/util.spec.js
@@ -1,4 +1,4 @@
-const { CheckIfArrayOrString } = require('../../src/wp-cli/util')
+const { CheckIfArrayOrString, FormatToWordpressDate } = require('../../src/wp-cli/util')
 
 describe('util', () => {
   describe('#CheckIfArrayOrString()', () => {
@@ -13,6 +13,20 @@ describe('util', () => {
 
     it('should throw an error with massage when item is not array or string', () => {
       expect(() => CheckIfArrayOrString(1, 'fail')).toThrow('fail must be an array or a string')
+    })
+  })
+
+  describe('#FormatToWordpressDate()', () => {
+    it('should return valid wordpress date with dates numbers smaller then 10', () => {
+      const date = new Date(2020, 5, 6, 2, 8, 3)
+
+      expect(FormatToWordpressDate(date)).toBe('2020-06-06-02-08-03')
+    })
+
+    it('should return valid wordpress date with dates numbers bigger then 10', () => {
+      const date = new Date(2020, 11, 20, 20, 40, 50)
+
+      expect(FormatToWordpressDate(date)).toBe('2020-12-20-20-40-50')
     })
   })
 })
