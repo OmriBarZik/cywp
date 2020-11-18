0c5b4c3ab8a8d68e5b6efe732574816672ba2ce0
Omri Bar-Zik <omri@bar-zik.com>
2020-11-18 14:55:04 +0200

    Update: moved eslint config to separate js file

---
+++ .eslintrc.js
@@ -0,0 +1,68 @@
+/* eslint-disable spellcheck/spell-checker */
+module.exports = {
+  extends: [
+    'standard',
+    'plugin:jsdoc/recommended',
+    'plugin:jest/style',
+    'plugin:jest/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    'jest',
+    'jsdoc',
+    'spellcheck',
+  ],
+  env: {
+    es6: true,
+    node: true,
+    'jest/globals': true,
+  },
+  rules: {
+    yoda: [
+      'error',
+      'always',
+    ],
+    'function-call-argument-newline': [
+      'error',
+      'never',
+    ],
+    'function-paren-newline': [
+      'error',
+      'never',
+    ],
+    'comma-dangle': [
+      'error',
+      {
+        arrays: 'always-multiline',
+        objects: 'always-multiline',
+      },
+    ],
+    'jsdoc/no-undefined-types': 'off',
+    'jsdoc/valid-types': 'off',
+    'spellcheck/spell-checker': [
+      'warn',
+      {
+        skipWords: [
+          'cywp',
+          'stdout',
+          'stderr',
+          'ps',
+          'wordpress',
+          'mysql',
+          'twentyseventeen',
+          'spwan',
+          'usr',
+          'cmd',
+          'wp',
+          'uri',
+          'php',
+          'nicename',
+          'unspam',
+        ],
+      },
+    ],
+  },
+}
