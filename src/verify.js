cd00d4065270f14e3c9838dddb728e3d0459902c
Omri Bar-Zik <omri@bar-zik.com>
2020-12-17 12:53:26 +0200

    Add: verify checks

--- src/verify.js
+++ src/verify.js
@@ -1,31 +1,63 @@
 const commandExists = require('command-exists')
-
-const missingDependencies = []
+const { ReturnPromise } = require('./docker/util')
+const { spawn } = require('child_process')
 
 /**
- * Checks whatever the given command exsists, if not. the name fo the
- * command will be added to the missingDependencies array
+ * Checks if docker is installed on the system.
  *
- * @param {string} command - the command
- * @returns {Promise<string>} - retrun if the command exsists.
+ * @returns {Promise<string>}
  */
-function verifyCommand (command) {
-  return commandExists(command).catch(() => missingDependencies.push(command))
+function verifyDocker () {
+  return commandExists('docker')
+    .catch(() => 'Docker is\'t installed on this system! Please install docker and try again.')
+}
+
+/**
+ * Checks if the docker demon is running.
+ *
+ * @returns {Promise<string>} return is the docker demon is running.
+ */
+function verifyDockerRunning () {
+  const stats = spawn('docker', ['stats', '--no-stream'])
+  return ReturnPromise(stats, () => { })
+    .catch(() => {
+      console.log('adasd')
+      return ('The docker demon is\'t running! please start the demon and try again.')
+    })
 }
 
 /**
  * Verify if the system have the right dependencies.
+ *
+ * @returns {Promise<boolean>} if the system can run cywp and exit if
  */
 async function verify () {
-  await Promise.all([verifyCommand('docker')])
+  return safeVerify()
+    .catch(err => {
+      console.error(err)
+      process.exit(1)
+    })
+}
 
-  if (missingDependencies.length) {
-    // eslint-disable-next-line no-console
-    console.error(`The dependencies [ ${missingDependencies.join(' | ')} ] are missing\nplease install them and try again`)
-    process.exit(1)
-  }
+/**
+ * Verify if the system have the right dependencies.
+ *
+ * @returns {Promise<boolean,string>}
+ */
+async function safeVerify () {
+  return Promise.all([verifyDocker(), verifyDockerRunning()])
+    .catch(results => {
+      for (let i = 0; i < results.length; i++) {
+        if (results[i]) { return results[i] }
+      }
+    })
+    .then((e) => {
+      console.log(e)
+      return true
+    })
 }
 
 module.exports = {
   verify,
+  verifyDockerRunning,
 }
