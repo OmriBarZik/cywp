7fd738d7ce91a6e63df0910d225346b3d5d46df2
Omri Bar-Zik <omri@bar-zik.com>
2021-04-25 20:32:36 +0300

    Update config

--- src/config.js
+++ src/config.js
@@ -1,5 +1,53 @@
-const { existsSync } = require('fs')
+const { existsSync, readFileSync } = require('fs')
 const { resolve, join } = require('path')
+
+/**
+ * checks if version is a valid version. i.g (x.y.z)
+ *
+ * @param {string} version checks if valid version.
+ * @returns {boolean} If a valid version.
+ */
+function validateVersion (version) {
+  return /^\d+(\.\d+){0,2}$/.test(version)
+}
+
+/**
+ * checks and set the wordpress version.
+ *
+ * @param {string} wordpressVersion raw user wordpress version.
+ * @returns {string} validate wordpress version.
+ */
+function setWordpressVersion (wordpressVersion) {
+  if (!wordpressVersion) {
+    console.info('wordpress version not found, using latest')
+    return 'latest'
+  }
+
+  if (!validateVersion(wordpressVersion)) {
+    throw new Error('invalid wordpress version! please check \'wordpressVersion\' and try again.')
+  }
+
+  return wordpressVersion
+}
+
+/**
+ * checks and validate wordpress port.
+ *
+ * @param {string | number} wordpressPort raw user wordpress port.
+ * @returns {number} validate wordpress port.
+ */
+function setWordpressPort (wordpressPort) {
+  if (!wordpressPort) {
+    console.warn('wordpress port was not provided using default port 8000')
+    return 8000
+  }
+
+  if (!Number.isInteger(+wordpressPort)) {
+    throw new Error('invalid wordpress port! please check \'wordpressPort\' and try again')
+  }
+
+  return wordpressPort
+}
 
 /**
  * checks and setup all cywp settings.
@@ -11,8 +59,8 @@ function checkConfig (config) {
   const configJson = require(config.configFile)
 
   const cywpConfig = {
-    cywpWordpressVersion: configJson.wordpressVersion,
-    cywpWordpressPort: configJson.wordpressPort,
+    cywpWordpressVersion: setWordpressVersion(configJson.wordpressVersion),
+    cywpWordpressPort: setWordpressPort(configJson.wordpressPort),
     cywpWordpressName: 'cywp-tmp-wordpress',
     cywpLocalPlugins: [],
     cywpRemotePlugins: [],
@@ -21,19 +69,9 @@ function checkConfig (config) {
     cywpThemePath: [],
   }
 
-  if (!configJson.wordpressVersion) {
-    console.warn('wordpress version not found, using latest')
-    cywpConfig.cywpWordpressVersion = 'latest'
-  }
-
   if (!configJson.wordpressTheme) {
     console.warn('wordpress theme was not provided using default theme twentytwenty')
     cywpConfig.cywpTheme = 'twentytwenty'
-  }
-
-  if (!configJson.wordpressPort) {
-    console.warn('wordpress port was not provided using default port 8000')
-    cywpConfig.cywpWordpressPort = 8000
   }
 
   if (configJson.wordpressPlugins) {
@@ -41,11 +79,17 @@ function checkConfig (config) {
       const pathOrVersion = configJson.wordpressPlugins[plugin]
 
       if (existsSync(pathOrVersion)) {
-        if (!existsSync(join(pathOrVersion, `${plugin}.php`))) {
-          throw new Error(`${plugin}.php not found in ${pathOrVersion}. pleas provide a valid plugin path and try again.`)
+        const pluginDir = resolve(pathOrVersion)
+        const pluginPath = join(pluginDir, `${plugin}.php`)
+        if (!existsSync(pluginPath)) {
+          throw new Error(`${plugin}.php not found in ${pluginDir}. pleas provide a valid plugin path and try again.`)
         }
 
-        cywpConfig.cywpLocalPlugins.push({ host: resolve(pathOrVersion), docker: `/var/www/html/wp-content/plugins/${plugin}:ro`, name: plugin })
+        if (!readFileSync(pluginPath).toString().match(new RegExp(`Plugin Name:.*${plugin}`))) {
+          throw new Error(pluginPath + 'must be a valid plugin, please note that the plugin name matches the cypress.json config.')
+        }
+
+        cywpConfig.cywpLocalPlugins.push({ host: pluginDir, docker: `/var/www/html/wp-content/plugins/${plugin}:ro`, name: plugin })
       } else {
         cywpConfig.cywpRemotePlugins.push({ name: plugin, version: pathOrVersion })
       }
