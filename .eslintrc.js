fb1102976f5a080d8a7739398cfdb2e7b3d20d33
Omri Bar-Zik <omri@bar-zik.com>
2021-04-29 00:15:36 +0300

    update bin

--- .eslintrc.js
+++ .eslintrc.js
@@ -58,6 +58,7 @@ module.exports = {
           'usr',
           'cmd',
           'wp',
+          'wpcli',
           'uri',
           'php',
           'nicename',
--- bin/cywp.js
+++ bin/cywp.js
@@ -6,30 +6,44 @@ const { program } = require('commander')
 
 const checkConfig = require('../src/config')
 const { Docker } = require('../src/docker/docker')
+const runner = require('../src/runner')
+const { CreateWordpressCliContainer } = require('../src/docker/presets/containers')
 
 const docker = new Docker()
+
+program
+  .option('-p, --project <project-path>', 'path to cypress project')
 
 program.command('exec')
   .arguments('<container> <command> [args...]')
   .description('execute command on a specified container', {
-    container: 'Container to run the command. (wordpress|mysql)',
+    container: 'Container to run the command. (wordpress|mysql|wpcli)',
     command: 'command to run on the container',
     args: 'command arguments',
   })
-  .option('-p, --project <project-path>', 'path to cypress project')
   .action(exec)
 
-program.command('clean')
-  .description('removes all project\'s docker containers, networks and volumes')
+program.command('rm [container]')
+  .description('removes all stopped containers, networks and volumes of the project', {
+    container: 'remove a specific container (wordpress|mysql|phpmyadmin)',
+  })
   .option('-a, --all', 'remove all containers, networks and volumes that starts with \'cywp-\' prefix')
-  .action(clean)
+  .option('-f, --force', 'removes all containers, networks and volumes, even running once')
+  .option('--no-volumes', 'do not delete connected volumes')
+  .action(rm)
 
-program.command('phpmyadmin')
-  .description('run phpmyadmin docker container to access MySQL server.')
-  .action(phpmyadmin)
+program.command('start [container]')
+  .description('start cypress-for-wordpress environment', {
+    container: 'create and start a specific container (wordpress|mysql|phpmyadmin)',
+  })
+  .action(start)
 
-program.command('setup')
-  .action(setup)
+program.command('stop [container]')
+  .description('stop all project\'s containers', {
+    container: 'stop a specific container (wordpress|mysql|phpmyadmin)',
+  })
+  .option('-a, --all', 'stop all containers that starts with \'cywp-\' prefix')
+  .action(stop)
 
 program.parse()
 
@@ -39,22 +53,14 @@ program.parse()
  * @param {string} container the container to run the command on.
  * @param {string} command command to pass to the container.
  * @param {string[]} args command arguments.
- * @param {object} options subCommand option object.
  * @returns {any} docker container.
  */
-function exec (container, command, args, options) {
-  const cypressPath = path.join(path.resolve(options.project || process.cwd()), 'cypress.json')
-  const config = checkConfig({ configFile: cypressPath, env: {} })
-
-  if (!fs.existsSync(cypressPath)) {
-    return console.error('cypress.json not found!' +
-      options.project ? 'check your path!'
-      : 'create cypress.json or use --path to direct to valid cypress project')
-  }
+async function exec (container, command, args) {
+  const config = getConfig()
 
   const containerCallback = (attachedContainer) => {
     if (!attachedContainer) {
-      return console.error('container not working, use "cywp setup" to start the containers')
+      return console.error('container not working, use \'cywp start\' to start the containers')
     }
     return attachedContainer.exec([command].concat(args))
   }
@@ -67,25 +73,57 @@ function exec (container, command, args, options) {
     return docker.AttachContainer({ name: config.env.cywpWordpressName }).then(containerCallback)
   }
 
-  return console.error('invalid container argument! must be wordpress of mysql')
+  if ('wpcli' === container) {
+    return docker.AttachContainer({ name: config.env.cywpWordpressName })
+      .then(wordpress => {
+        if (wordpress) {
+          return CreateWordpressCliContainer(wordpress, [command].concat(args))
+        }
+
+        console.log('wordpress not running! please start wordpress to use wpcli')
+      })
+  }
+
+  return console.error('invalid container argument! container must be \'wordpress\', \'mysql\' or \'wpcli\'!')
 }
 
 /**
  * @param {{all:string}} options - clean command option object.
  */
-function clean (options) {
+function rm (options) {
   console.log('clean project ' + options.all)
 }
 
 /**
- *
+ * ret
  */
-function setup () {
-
+function start () {
+  const config = getConfig()
+  runner(() => { }, config)
 }
 
 /**
  *
  */
-function phpmyadmin () {
+function stop () {
+
+}
+
+/**
+ * get cypress config from cypress.json file
+ *
+ * @returns {object} cypress config
+ */
+function getConfig () {
+  const cypressPath = path.join(path.resolve(program.project || process.cwd()), 'cypress.json')
+
+  if (!fs.existsSync(cypressPath)) {
+    const errorMassage = `cypress.json was not found in ${path.dirname(cypressPath)}!`
+    const userHelper = program.project ? 'please check the \'--project\' path!'
+      : 'create cypress.json or use --path to direct to a valid cypress project'
+
+    return console.error(`${errorMassage}\n${userHelper}`)
+  }
+
+  return checkConfig({ configFile: cypressPath, env: {} })
 }
