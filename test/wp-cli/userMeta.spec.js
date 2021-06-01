8089f459d318c227eb38daf0f0c748f5bf08443e
Omri Bar-Zik <omri@bar-zik.com>
2021-06-01 06:17:39 +0300

    Fix Tests

--- test/wp-cli/userMeta.spec.js
+++ test/wp-cli/userMeta.spec.js
@@ -3,7 +3,7 @@ const UserMeta = require('../../src/wp-cli/userMeta')
 jest.mock('../../src/docker/presets/containers')
 
 describe('#UserMeta', () => {
-  /** @type {User} */
+  /** @type {UserMeta} */
   let userMeta
 
   beforeAll(() => {
@@ -11,55 +11,55 @@ describe('#UserMeta', () => {
   })
 
   beforeEach(async () => {
-    userMeta.wpUser = jest.fn()
+    userMeta.wpUserMeta = jest.fn()
   })
 
   describe('##add()', () => {
     it('should have arguments to add user metadata', () => {
-      userMeta.Meta.add(1, 'add-metadata-key', 'add-metadata-value')
+      userMeta.add(1, 'add-metadata-key', 'add-metadata-value')
 
-      expect(userMeta.wpUser)
-        .toHaveBeenLastCalledWith(['meta', 'add', 1, 'add-metadata-key', 'add-metadata-value'])
+      expect(userMeta.wpUserMeta)
+        .toHaveBeenLastCalledWith(['add', 1, 'add-metadata-key', 'add-metadata-value'])
     })
   })
 
   describe('##delete()', () => {
     it('should have arguments to delete user metadata', () => {
-      userMeta.Meta.delete(1, 'delete-metadata-key')
+      userMeta.delete(1, 'delete-metadata-key')
 
-      expect(userMeta.wpUser)
-        .toHaveBeenLastCalledWith(['meta', 'delete', 1, 'delete-metadata-key'])
+      expect(userMeta.wpUserMeta)
+        .toHaveBeenLastCalledWith(['delete', 1, 'delete-metadata-key'])
     })
   })
 
   describe('##get()', () => {
     it('should have arguments to get user metadata', () => {
-      userMeta.wpUser = jest.fn((commands) => Promise.resolve({ stdout: JSON.stringify(commands) }))
+      userMeta.wpUserMeta = jest.fn((commands) => Promise.resolve({ stdout: JSON.stringify(commands) }))
 
-      userMeta.Meta.get(1, 'get-metadata-key')
+      userMeta.get(1, 'get-metadata-key')
 
-      expect(userMeta.wpUser)
-        .toHaveBeenLastCalledWith(['meta', 'get', '--format=json', 1, 'get-metadata-key'])
+      expect(userMeta.wpUserMeta)
+        .toHaveBeenLastCalledWith(['get', '--format=json', 1, 'get-metadata-key'])
     })
   })
 
   describe('##list()', () => {
     it('should have arguments to list user metadata', () => {
-      userMeta.wpUser = jest.fn((commands) => Promise.resolve({ stdout: JSON.stringify(commands) }))
+      userMeta.wpUserMeta = jest.fn((commands) => Promise.resolve({ stdout: JSON.stringify(commands) }))
 
-      userMeta.Meta.list(1)
+      userMeta.list(1)
 
-      expect(userMeta.wpUser)
-        .toHaveBeenLastCalledWith(['meta', 'list', '--format=json', 1])
+      expect(userMeta.wpUserMeta)
+        .toHaveBeenLastCalledWith(['list', '--format=json', 1])
     })
   })
 
   describe('##update()', () => {
     it('should have arguments to update user metadata', () => {
-      userMeta.Meta.update(1, 'update-metadata-key', 'update-metadata-value')
+      userMeta.update(1, 'update-metadata-key', 'update-metadata-value')
 
-      expect(userMeta.wpUser)
-        .toHaveBeenLastCalledWith(['meta', 'update', 1, 'update-metadata-key', 'update-metadata-value'])
+      expect(userMeta.wpUserMeta)
+        .toHaveBeenLastCalledWith(['update', 1, 'update-metadata-key', 'update-metadata-value'])
     })
   })
 })
